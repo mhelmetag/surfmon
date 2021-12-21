eec528154fe1e4492e9be72cbc5be5c1fb825735
Max Helmetag <mhelmetag@gmail.com>
2021-12-20 23:40:28 -0500

    rubocop

--- test/models/alert_test.rb
+++ test/models/alert_test.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # == Schema Information
 #
 # Table name: alerts
@@ -9,7 +11,7 @@
 #  updated_at :datetime         not null
 #
 
-require "test_helper"
+require 'test_helper'
 
 class AlertTest < ActiveSupport::TestCase
   # test "the truth" do
