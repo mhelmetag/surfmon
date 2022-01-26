e95104b89bf8967e76b986cf0cc6cae93f8d7887
Max Helmetag <mhelmetag@gmail.com>
2022-01-25 22:31:00 -0800

    use stimulus for navbar

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,15 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-
-window.toggleNavBar = () => {
-  const $navbarBurger = document.querySelector(".navbar-burger")
-
-  if ($navbarBurger) {
-    const targetId = $navbarBurger.dataset.target
-    const $navMenu = document.getElementById(targetId)
-
-    $navbarBurger.classList.toggle('is-active')
-    $navMenu.classList.toggle('is-active')
-  }
-}
+import "@hotwired/turbo-rails";
+import "controllers";
