63f545ee1841021df85c49a2aa2a371f1109a6f0
Max Helmetag <mhelmetag@gmail.com>
2022-02-03 17:50:25 -0800

    chill on rubocop

--- lib/alerts/sources/wave_source.rb
+++ lib/alerts/sources/wave_source.rb
@@ -29,7 +29,7 @@ module Alerts
       end
 
       def options
-        { query: { subregionId: subregion_id, days: 8 }, headers: }
+        { query: { subregionId: subregion_id, days: 8 }, headers: headers }
       end
 
       def headers
