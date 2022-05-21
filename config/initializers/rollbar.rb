c004dce100f7b4d62fd2be9e44149c1cc9a807d2
Max Helmetag <mhelmetag@gmail.com>
2022-05-20 23:53:53 -0700

    rubocop

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rollbar.configure do |config|
-  config.access_token = ENV['ROLLBAR_ACCESS_TOKEN']
+  config.access_token = ENV.fetch('ROLLBAR_ACCESS_TOKEN', nil)
 
   # Here we'll disable in 'test' and 'development':
   config.enabled = false if Rails.env.test? || Rails.env.development?
