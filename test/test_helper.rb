fe324d199ec46c872e8f104353de6f5f9223fa35
Max Helmetag <mhelmetag@gmail.com>
2022-09-02 23:35:24 -0700

    alerts e2e tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,8 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+require 'passwordless/test_helpers'
+
 module ActiveSupport
   class TestCase
     # Run tests in parallel with specified workers
@@ -13,5 +15,6 @@ module ActiveSupport
     fixtures :all
 
     # Add more helper methods to be used by all tests here...
+    Capybara.default_max_wait_time = 2
   end
 end
