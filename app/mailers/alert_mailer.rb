875f54db10e291aaea2a16f925b7c78d283b7bbd
Max Helmetag <mhelmetag@gmail.com>
2023-09-14 22:50:23 -0700

    5 day reports

--- app/mailers/alert_mailer.rb
+++ app/mailers/alert_mailer.rb
@@ -6,7 +6,7 @@ class AlertMailer < ApplicationMailer
     @alert_names_and_days_of_week = alert_ids_and_days_of_week.map do |alert_id, days_of_week|
       alert = Alert.find(alert_id)
 
-      [alert.name, days_of_week.map { |n| n.days.from_now.strftime('%A') }.join(', ')]
+      [alert.name, days_of_week.map { |n| (n - 1).days.from_now.strftime('%A') }.join(', ')]
     end
     mail(to: @user.email, subject: 'Surfmon weekly surf alerts 🏄')
   end
